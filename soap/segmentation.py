4fa3f5c4f94a80aa2babb57d579e3f05bce0df1d
mjdall <morgandally4@gmail.com>
2021-05-23 17:20:48 +1200

    Add Image Masking

--- soap/segmentation.py
+++ soap/segmentation.py
@@ -11,6 +11,7 @@ class Segmenter():
     def __init__(self, x_width=-1):
         self.images = None
         self.gray_images = None
+        self.image_masks = None
         pass
 
 
@@ -76,7 +77,7 @@ class Segmenter():
 
 
     def _parse_gray_images(self):
-        if self.gray is None:
+        if self.gray_images is None:
             print("parsing images to gray scale first")
             self._parse_to_gray()
         
@@ -84,7 +85,37 @@ class Segmenter():
         for image in self.gray_images:
             dask_computation.append(dask.delayed(self._parse_gray_image)(image))
 
-        self.gray_pixels = dask.compute(dask_computation)
+        self.gray_pixels = dask.compute(dask_computation)[0]
+
+
+    def _create_mask(self, gray_image, gray_filter=.55, use_mean=False):
+        image_copy = gray_image.copy()
+        image_mean = image_copy.reshape(
+            image_copy.shape[0]*image_copy.shape[1]).mean()
+
+        for y in range(gray_image.shape[0]):
+            for x in range(gray_image.shape[1]):
+                pixel_val = gray_image[y][x]
+                if ((pixel_val > gray_filter and not use_mean)
+                    or (use_mean and pixel_val > image_mean)):
+                    image_copy[y][x] = 1
+
+        return(image_copy)
+
+
+    def create_masks(self, use_mean=False):
+        if self.gray_images is None:
+            print("parsing images to gray scale first")
+            self._parse_to_gray()
+
+        print("Creating image masks")
+
+        dask_computation = []
+        for image in self.gray_images:
+            dask_computation.append(
+                dask.delayed(self._create_mask)(image, use_mean=use_mean))
+
+        self.image_masks = dask.compute(dask_computation)[0]
 
 
     def plot_images(self, gray=False):
@@ -97,22 +128,34 @@ class Segmenter():
 
     
     def save_images(self, prefix="image",
-                    output_dir="output\\resized", gray=False):
+                    output_dir="output", image_type="resized"):
         images_to_save = self.images
-        if gray:
-            images_to_save = self.gray_images
-            if output_dir == "output\\resized":
-                output_dir = "output\\gray"
+        save_mapping = {
+            "resized": { "images": self.images, "cmap": None },
+            "gray": { "images": self.gray_images, "cmap": plt.get_cmap("gray") },
+            "masks": { "images": self.image_masks, "cmap": plt.get_cmap("gray") },
+        }
+
+        if image_type not in save_mapping:
+            type_str = ",".join(save_mapping.keys())
+            raise RuntimeError(f"`image_type` must be one of: {type_str}")
+
+        output_dir = f"{output_dir}\\{image_type}"
+        images_to_save = save_mapping[image_type]["images"]
+        image_cmap = save_mapping[image_type]["cmap"]
+
+        if images_to_save is None:
+            raise RuntimeError(f"{image_type} hasn't been created yet")
 
         os.makedirs(output_dir, exist_ok=True)
 
         saved_images = []
         for i, image in enumerate(images_to_save):
             image_filename = os.path.join(output_dir, f"{prefix}_{i}.jpg")
-            cmap = plt.get_cmap("gray") if gray else None
-            plt.imsave(image_filename, image, cmap=cmap)
+            plt.imsave(image_filename, image, cmap=image_cmap)
 
             saved_images.append(image_filename)
             print(f"image saved to: {image_filename}")
 
         return(saved_images)
+
--- test.py
+++ test.py
@@ -4,10 +4,18 @@ segmenter = segmentation.Segmenter()
 
 segmenter.load_images_from_directory("data")
 
-segmenter.rescale_images(500)
+segmenter.rescale_images(1000)
 
 segmenter.save_images()
 
 segmenter._parse_to_gray()
 
-segmenter.save_images(gray=True)
+segmenter.save_images(image_type="gray")
+
+segmenter.create_masks()
+
+segmenter.save_images(image_type="masks")
+
+# segmenter.create_masks(use_mean=True)
+
+# segmenter.save_images(image_type="masks")
